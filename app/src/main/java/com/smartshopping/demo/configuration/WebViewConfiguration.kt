d9abf582d5d1e2c54437557406267543ab561737
lenadweb <90589140+lenadweb@users.noreply.github.com>
2023-03-30 20:24:57 +0400

    fix webView configuration

--- app/src/main/java/com/smartshopping/demo/configuration/WebViewConfiguration.kt
+++ app/src/main/java/com/smartshopping/demo/configuration/WebViewConfiguration.kt
@@ -1,6 +1,9 @@
 package com.smartshopping.demo.configuration
 
 import android.graphics.Bitmap
+import android.view.View
+import android.webkit.CookieManager
+import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import com.smartshopping.module.SmartShopping
@@ -36,8 +39,27 @@ class WebViewConfiguration {
                     }
                 }
             }
+            CookieManager.getInstance().setAcceptThirdPartyCookies(webView, true)
+            webView.settings.userAgentString = "Mozilla/5.0 (Linux; Android 11; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.91 Mobile Safari/537.36"
+            webView.settings.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+            webView.settings.cacheMode = WebSettings.LOAD_NO_CACHE
+            webView.setLayerType(View.LAYER_TYPE_SOFTWARE, null)
             webView.settings.javaScriptEnabled = true
             webView.loadUrl("https://www.stockx.com")
+            webView.settings.databaseEnabled = true
+            webView.settings.domStorageEnabled = true
+            webView.settings.allowFileAccess = true
+            webView.settings.allowFileAccessFromFileURLs = true
+            webView.settings.allowContentAccess = true
+            webView.settings.loadWithOverviewMode = true
+            webView.settings.cacheMode = WebSettings.LOAD_DEFAULT
+            webView.settings.setRenderPriority(WebSettings.RenderPriority.HIGH)
+            webView.settings.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+            webView.settings.textZoom = 100
+            webView.settings.layoutAlgorithm = WebSettings.LayoutAlgorithm.TEXT_AUTOSIZING
+            webView.setLayerType(WebView.LAYER_TYPE_NONE, null)
+            webView.settings.setSupportMultipleWindows(false)
+
             WebView.setWebContentsDebuggingEnabled(true)
             smartShopping.install(webView)
         }
